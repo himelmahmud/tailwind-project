f733f1b605c2eb2ebce2f7f09d3e97e6d528fa4d
Himel Mahmud <91639059+himelmahmud@users.noreply.github.com>
2024-10-12 19:47:57 +0600

    git add remote vercel test

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 const btn = document.getElementById("menu-btn");
 const nav = document.getElementById("menu");
 
+let x = "git test";
+
 btn.addEventListener("click", () => {
   btn.classList.toggle("open");
   nav.classList.toggle("flex");
