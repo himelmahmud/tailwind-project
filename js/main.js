648351b0a6d0f7a21c69b510764ff30a5bf09ca7
Himel Mahmud <91639059+himelmahmud@users.noreply.github.com>
2024-10-12 20:03:31 +0600

    finish website

--- js/main.js
+++ js/main.js
@@ -1,8 +1,6 @@
 const btn = document.getElementById("menu-btn");
 const nav = document.getElementById("menu");
 
-let x = "git test";
-
 btn.addEventListener("click", () => {
   btn.classList.toggle("open");
   nav.classList.toggle("flex");
